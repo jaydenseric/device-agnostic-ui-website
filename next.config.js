1771cfd4eb534ba4e6a431de896994147a17464b
Jayden Seric <me@jaydenseric.com>
2019-10-31 19:05:39 +1100

    Ensure device-agnostic-ui isn’t duplicated accross page bundles.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@ module.exports = {
   env: {
     ORIGIN: process.env.ORIGIN
   },
+  webpack(config, { isServer }) {
+    if (!isServer)
+      config.optimization.splitChunks.cacheGroups['device-agnostic-ui'] = {
+        name: 'device-agnostic-ui',
+        test: /[\\/]node_modules[\\/]device-agnostic-ui[\\/]/,
+        reuseExistingChunk: true,
+        priority: 50
+      }
+
+    return config
+  },
   experimental: {
     deferScripts: true,
     granularChunks: true
