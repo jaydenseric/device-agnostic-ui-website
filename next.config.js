1a1a9a9a7d4dbb0a97c89bcc37c252f60d228bd0
Jayden Seric <me@jaydenseric.com>
2021-02-08 22:32:17 +1100

    Simplify Next.js config.
    `granularChunks` is no longer and experimental option.

--- next.config.js
+++ next.config.js
@@ -2,18 +2,4 @@ module.exports = {
   env: {
     ORIGIN: process.env.ORIGIN || `https://${process.env.VERCEL_URL}`,
   },
-  webpack(config, { isServer }) {
-    if (!isServer)
-      config.optimization.splitChunks.cacheGroups['device-agnostic-ui'] = {
-        name: 'device-agnostic-ui',
-        test: /[\\/]node_modules[\\/]device-agnostic-ui[\\/]/u,
-        reuseExistingChunk: true,
-        priority: 50,
-      };
-
-    return config;
-  },
-  experimental: {
-    granularChunks: true,
-  },
 };
